45130686b418f5833a07b23aee454f6239efef66
Donuts Diaz <159488665+AdonaiDiazEsparza@users.noreply.github.com>
2026-02-11 12:39:10 -0600

    refactor: build project

--- ProjectFiles/MyDrvr/DrvrDefs.h
+++ ProjectFiles/MyDrvr/DrvrDefs.h
@@ -3,8 +3,19 @@
 #include <Ntifs.h>
 #include <ntddk.h>
 #include <wdf.h>
+#include <ntimage.h>
 
 #define DRIVER_PREFIX "=> DRIVER_TEST: " // Prefix for the logs
+
+// Direcciones de las DLL a inyectar, dll para un proceso nativo o para un proceso de 32bit en uno de 64
+#define DLL_PATH_NATIVE L"PATH"
+#define DLL_PATH_WOW64 L""
+
+// Rutas de NTDLL en un proceso nativo o proceso de 32bit en uno de 64
+#define NTDLL_NATIVE_PATH L"System32\\ntdll.dll"
+#define NTDLL_WOW64_PATH L"SysWOW64\\ntdll.dll"
+
+#define TAG_INJ 'jnI'
 
 /* Macro for print with line jump */
 #define PRINT(fmt, ...) \
@@ -16,12 +27,12 @@
 // Flipea la TAG
 #define TAG(t) ( ((((ULONG)t) & 0xFF) << (8 * 3)) | ((((ULONG)t) & 0xFF00) << (8 * 1)) | ((((ULONG)t) & 0xFF0000) >> (8 * 1)) | ((((ULONG)t) & 0xFF000000) >> (8 * 3)) )
 
-// Rutas definidas para las DLL
-#define EDRDLL_PATH L"C:\\test\\edrHook.dll"
-
 // MACROS DE APOYO
 #define STATIC_UNICODE_STRING(name, str) \
     static const UNICODE_STRING name = RTL_CONSTANT_STRING(str)
+
+#define SET_UNICODE_STRING(name,str)\
+    UNICODE_STRING name = RTL_CONSTANT_STRING(str)
 
 #define STATIC_OBJECT_ATTRIBUTES(object_attributes, label_name, str_name)\
 	STATIC_UNICODE_STRING(label_name, str_name);\
@@ -30,6 +41,35 @@
 #define GET_PEPROCESS(peproc, pid)\
 	PEPROCESS peproc = NULL;\
 	PsLookupProcessByProcessId(pid, &peproc)
+
+typedef struct _INJECTION_INFO
+{
+    /**
+     * @brief este es usado para crear el nodo en la lista global
+     */
+    LIST_ENTRY entry;
+
+    /**
+     * @brief variable que almacena el id del proceso
+     */
+    HANDLE ProcessId;
+
+    /**
+     * @brief esta variable es usada para indicar si la DLL ya fue inyectada
+     */
+    BOOLEAN isInjected;
+
+    /**
+     * @brief esta variable indica si es un proceso de 32bit ejecutado en uno de 64bits
+     */
+    BOOLEAN is32BitProcess;
+
+    /**
+     * @brief Direccion de la rutina de DLL que se va usar
+     */
+    PVOID LdrLoadDllRoutineAddress;
+
+}INJECTION_INFO, * PINJECTION_INFO;
 
 // Funciones que se usaran para filtrar
 BOOLEAN IsSuffixedUnicodeString(PCUNICODE_STRING FullName, PCUNICODE_STRING ShortName, BOOLEAN CaseInsensitive);
@@ -46,13 +86,13 @@ enum KAPC_ENVIRONMENT
 };
 
 /**
- * 
+ *
  * En este punto del codigo se definen las funciones que se incluyen externamente en el desarrollo de Kernel
  * Se ocupan para funciones APC y externas que se deben importar para el funcionamiento de este driver
- * 
+ *
  */
 
-/* Definimos los tipos de dato a pasar a las rutinas de Ke */
+ /* Definimos los tipos de dato a pasar a las rutinas de Ke */
 typedef VOID(NTAPI* PKNORMAL_ROUTINE)(PVOID NormalContext, PVOID SystemArgument1, PVOID SystemArgument2);
 typedef VOID KKERNEL_ROUTINE(PRKAPC Apc, PKNORMAL_ROUTINE* NormalRoutine, PVOID* NormalContext, PVOID* SystemArgument1, PVOID* SystemArgument2);
 typedef KKERNEL_ROUTINE(NTAPI* PKKERNEL_ROUTINE);
@@ -81,26 +121,26 @@ extern "C" {
     );
 
     NTKERNELAPI
-    BOOLEAN
-    KeTestAlertThread(
-        KPROCESSOR_MODE AlertMode
-    );
+        BOOLEAN
+        KeTestAlertThread(
+            KPROCESSOR_MODE AlertMode
+        );
 
     /* Funciones para obtener Procesos */
     NTKERNELAPI PPEB __stdcall PsGetProcessPeb(_In_ PEPROCESS Process);
     NTKERNELAPI PVOID PsGetProcessWow64Process(__in PEPROCESS Process);
     NTKERNELAPI BOOLEAN __stdcall PsIsProtectedProcess(PEPROCESS Process);
-    NTKERNELAPI PCHAR __stdcall PsGetProcessImageFileName( _In_ PEPROCESS Process );
+    NTKERNELAPI PCHAR __stdcall PsGetProcessImageFileName(_In_ PEPROCESS Process);
 
     // Funcion para conseguir un directorio
     NTSYSAPI
-    PVOID
-    NTAPI
-    RtlImageDirectoryEntryToData(
-        _In_ PVOID BaseOfImage,
-        _In_ BOOLEAN MappedAsImage,
-        _In_ USHORT DirectoryEntry,
-        _Out_ PULONG Size
-    );
+        PVOID
+        NTAPI
+        RtlImageDirectoryEntryToData(
+            _In_ PVOID BaseOfImage,
+            _In_ BOOLEAN MappedAsImage,
+            _In_ USHORT DirectoryEntry,
+            _Out_ PULONG Size
+        );
 
 }
--- ProjectFiles/MyDrvr/Source.cpp
+++ ProjectFiles/MyDrvr/Source.cpp
@@ -1,101 +1,447 @@
 /*
 * Este es un archvio para el desarrollo de DLL y driver en conjunto
-* 
+*
 * Posiblemente me funcione este codigo como template para un futuro
 */
 
-#include "HeaderInjection.h"
+#include "DrvrDefs.h"
 
-// Vamos a usar dos rutinas
-// 1 para detectar cuando se realice la carga de DLL
-// 2 Para ver que coincidan las cargas de memoria
+LIST_ENTRY g_list_entry; // Esta variable contiene todos los nodos de informacion que se usaran para cada proceso
 
+ANSI_STRING LdrLoadDLLRoutineName = RTL_CONSTANT_STRING("LdrLoadDll"); // Funcion que buscamos en NTDLL para poder inyectar nuestra DLL
 
-// Funcion que se manda a llamar cada que una DLL se carga
-// Routine to detect when a DLL is loaded
-void LoadDLLNotify(PUNICODE_STRING imageName, HANDLE pid, PIMAGE_INFO imageInfo)
+// =====================================================================================
+
+void InitilizeInfoList();
+
+NTSTATUS CreateInfo(HANDLE ProcessId);
+
+BOOLEAN RemoveInfoByProcess(HANDLE ProcessId);
+
+PINJECTION_INFO FindInfoElement(HANDLE ProcessId);
+
+PVOID RtlxFindExportedRoutineByName(PVOID DllBase, PANSI_STRING ExportName);
+
+BOOLEAN CanBeInjected(PINJECTION_INFO info);
+
+PVOID RtlxFindExportedRoutineByName(PVOID DllBase, PANSI_STRING ExportName);
+
+// =====================================================================================
+
+/**
+ * @brief Funcion para inicializar la lista de ```g_list_entry```
+ */
+void InitilizeInfoList()
 {
-    UNREFERENCED_PARAMETER(imageInfo);
-    UNREFERENCED_PARAMETER(imageName);
-    UNREFERENCED_PARAMETER(pid);
+    InitializeListHead(&g_list_entry);
 
-    if (!imageName || !imageName->Buffer)
+    PRINT("[+] LISTA INICIALIZADA");
+}
+
+
+/**
+  * @brief Funcion para crear la informacion de Process Id
+  *
+  * @param ProcessId El Pid del proceso
+  */
+NTSTATUS CreateInfo(HANDLE ProcessId)
+{
+    PINJECTION_INFO InfoCreated = (PINJECTION_INFO)ExAllocatePool2(POOL_FLAG_NON_PAGED, sizeof(INJECTION_INFO), TAG_INJ);
+
+    if (!InfoCreated)
+    {
+        PRINT("[-] ERROR CRETING MEMORY: 0x%x", STATUS_MEMORY_NOT_ALLOCATED);
+        return STATUS_MEMORY_NOT_ALLOCATED;
+    }
+
+    RtlZeroMemory(InfoCreated, sizeof(INJECTION_INFO));
+
+    InfoCreated->ProcessId = ProcessId;
+
+    /* Aun no se si funcione esta implementacion en esta parte */
+    InfoCreated->is32BitProcess = IoIs32bitProcess(NULL);
+
+    if (InfoCreated->is32BitProcess)
+    {
+        PRINT("[.] Es un proceso de 32Bit");
+    }
+
+    InsertTailList(&g_list_entry, &InfoCreated->entry);
+
+    return STATUS_SUCCESS;
+}
+
+
+/**
+ * @brief Funcion para eliminar el espacio de memoria asignado para cierto proceso
+ *
+ * @param ProcessId Numero del proceso al que se eliminara el proceso
+ */
+BOOLEAN RemoveInfoByProcess(HANDLE ProcessId)
+{
+    PINJECTION_INFO info = FindInfoElement(ProcessId);
+
+    // Si retorna un puntero nulo
+    if (!info)
+    {
+        PRINT("[-] Informacion no conseguida del proceso no existente");
+        return FALSE;
+    }
+
+    // Remove list entry
+    RemoveEntryList(&info->entry);
+
+    // Liberar memoria
+    ExFreePool(info);
+
+    // Si no se libero la memoria
+    if (info) {
+        PRINT("[-] Informacion no liberada");
+        return FALSE;
+    }
+
+    return TRUE;
+}
+
+
+/**
+ * @brief Funcion para obtener la informacion de la lista en la DLL segun el proceso que se le haya dado
+ *
+ * @param ProcessId Encontrar la informacion en este proceso
+ */
+PINJECTION_INFO FindInfoElement(HANDLE ProcessId)
+{
+    PLIST_ENTRY NextEntry = g_list_entry.Flink;
+
+    while (NextEntry != &g_list_entry)
+    {
+        PINJECTION_INFO info = CONTAINING_RECORD(NextEntry, INJECTION_INFO, entry);
+
+        if (info->ProcessId == ProcessId)
+        {
+            return info;
+        }
+    }
+
+    return NULL;
+}
+
+
+/**
+ * @brief Funcion para saber si la DLL puede ser inyectada
+ *
+ * @param info Informacion a Inyectar
+ */
+BOOLEAN CanBeInjected(PINJECTION_INFO info)
+{
+    // Si el puntero es nulo, retorna un un falso
+    if (!info)
+    {
+        return FALSE;
+    }
+
+    if (info->LdrLoadDllRoutineAddress) {
+        PRINT("[.] Direccion de Rutina de Load ya asignado");
+        return FALSE;
+    }
+
+    return TRUE;
+}
+
+
+/**
+ * @brief Funcion para poder exportar la direccion de la
+ *
+ * @param DllBase La direccion de la DLL que se va tomar
+ *
+ * @param ExportName Nombre de la DLL
+ */
+PVOID RtlxFindExportedRoutineByName(PVOID DllBase, PANSI_STRING ExportName)
+{
+
+    PULONG NameTable;
+    PUSHORT OrdinalTable;
+    PIMAGE_EXPORT_DIRECTORY ExportDirectory;
+    LONG Low = 0, Mid = 0, High, Ret;
+    USHORT Ordinal;
+    PVOID Function;
+    ULONG ExportSize;
+    PULONG ExportTable;
+
+    ExportDirectory = (PIMAGE_EXPORT_DIRECTORY)RtlImageDirectoryEntryToData(DllBase, TRUE,
+        IMAGE_DIRECTORY_ENTRY_EXPORT,
+        &ExportSize);
+
+    if (!ExportDirectory)
+    {
+        return NULL;
+    }
+
+    NameTable = (PULONG)((ULONG_PTR)DllBase + ExportDirectory->AddressOfNames);
+    OrdinalTable = (PUSHORT)((ULONG_PTR)DllBase + ExportDirectory->AddressOfNameOrdinals);
+
+    High = ExportDirectory->NumberOfNames - 1;
+    while (High >= Low)
+    {
+        Mid = (Low + High) >> 1;
+
+        Ret = strcmp(ExportName->Buffer, (PCHAR)DllBase + NameTable[Mid]);
+        if (Ret < 0)
+        {
+            High = Mid - 1;
+        }
+        else if (Ret > 0)
+        {
+            Low = Mid + 1;
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    if (High < Low)
+    {
+        return NULL;
+    }
+
+    Ordinal = OrdinalTable[Mid];
+
+    if (Ordinal >= ExportDirectory->NumberOfFunctions)
+    {
+        return NULL;
+    }
+
+    ExportTable = (PULONG)((ULONG_PTR)DllBase + ExportDirectory->AddressOfFunctions);
+    Function = (PVOID)((ULONG_PTR)DllBase + ExportTable[Ordinal]);
+
+    NT_ASSERT(
+        (Function < (PVOID)ExportDirectory) ||
+        (Function > (PVOID)((ULONG_PTR)ExportDirectory + ExportSize)));
+
+    return Function;
+}
+
+// ========================================================================================================
+
+// Funcion para comparar un nombre corto con el nombre completo de la llamada a funcion
+BOOLEAN IsSuffixedUnicodeString(PCUNICODE_STRING FullName, PCUNICODE_STRING ShortName, BOOLEAN CaseInsensitive) {
+    if (FullName &&
+        ShortName &&
+        ShortName->Length <= FullName->Length)
+    {
+        UNICODE_STRING ustr = {
+            ShortName->Length,
+            ustr.Length,
+            (PWSTR)RtlOffsetToPointer(FullName->Buffer, FullName->Length - ustr.Length)
+        };
+
+        return RtlEqualUnicodeString(&ustr, ShortName, CaseInsensitive);
+    }
+
+    return FALSE;
+}
+
+// Funcion para mapear si la DLL fue cargada con la funcion LdrLoadDLL
+BOOLEAN IsMappedByLdrLoadDll(PCUNICODE_STRING ShortName)
+{
+    //Check if this thread runs from within LdrLoadDll() function for the 'ShortName' module.
+    //INFO: Otherwise the call could have come from someone invoking ZwMapViewOfSection with SEC_IMAGE
+    //      Ex: smss.exe can map kernel32.dll during creation of \\KnownDlls (in that case ArbitraryUserPointer will be 0)
+    //      ex: WOW64 processes map kernel32.dll several times (32 and 64-bit version) with WOW64_IMAGE_SECTION or NOT_AN_IMAGE
+    //RETURN:
+    //		- TRUE if yes
+    UNICODE_STRING Name;
+
+    __try
+    {
+        PNT_TIB Teb = (PNT_TIB)PsGetCurrentThreadTeb();
+        if (!Teb ||
+            !Teb->ArbitraryUserPointer)
+        {
+            //This is not it
+            return FALSE;
+        }
+
+        Name.Buffer = (PWSTR)Teb->ArbitraryUserPointer;
+
+        //Check that we have a valid user-mode address
+        ProbeForRead(Name.Buffer, sizeof(WCHAR), __alignof(WCHAR));
+
+        //Check buffer length
+        Name.Length = (USHORT)wcsnlen(Name.Buffer, MAXSHORT);
+        if (Name.Length == MAXSHORT)
+        {
+            //Name is too long
+            return FALSE;
+        }
+
+        Name.Length *= sizeof(WCHAR);
+        Name.MaximumLength = Name.Length;
+
+        //See if it's our needed module
+        return IsSuffixedUnicodeString(&Name, ShortName, TRUE);
+    }
+    __except (EXCEPTION_EXECUTE_HANDLER)
+    {
+        //Something failed
+        PRINT("#EXCEPTION: (0x%X) IsMappedByLdrLoadDll", GetExceptionCode());
+    }
+
+    return FALSE;
+}
+
+// ========================================================================================================
+
+/**
+ * @brief Rutina para la captura de imagen
+ *
+ * @param ImageName Nombre de la imagen o DLL cargada
+ *
+ * @param ProcessId Es un handle y contiene el id del proceso que carga la DLL
+ *
+ * @param ImageInfo Informacion de la DLL cargada
+ */
+void NotifyForAImageLoaded(PUNICODE_STRING ImageName, HANDLE ProcessId, PIMAGE_INFO ImageInfo)
+{
+    if (!ImageName || !ImageName->Buffer)
         return;
 
-	GET_PEPROCESS(process, pid);
+    // Encontrar la informacion
+    PINJECTION_INFO info = FindInfoElement(ProcessId);
 
-    /* Voy a agregar una funcion para no filtrar */
-
-	PRINT("NAME PROCESS: %d IMAGE: %wZ", pid, imageName);
-
-    if(PsIsProtectedProcess(process)){
-        PRINT("[!] PROCESO PROTEGIDO");
+    // Si retorna un NULL 
+    if (info == NULL) {
+        PRINT("[!] Informacion no obtenida para este proceso");
+        return;
     }
 
+    // Obtienes el proceso
+    GET_PEPROCESS(process, ProcessId);
 
-}
+    // Revisa si el proceso esta protegido
+    if (PsIsProtectedProcess(process) && info->is32BitProcess && ImageInfo->SystemModeImage) // Por el momento agregare el filtro si es un proceso de 32bit 
+    {
+        if (RemoveInfoByProcess(ProcessId)) {
+            PRINT("[.] Informacion removida de este proceso protegido %d", ProcessId);
+        }
+        return;
+    }
 
-// Rutina para la creacion de procesos
-void CreateNotifyRoutine(HANDLE ppid, HANDLE pid, BOOLEAN create) {
-	if (create) {
-		PRINT("[!] PROCESS PARENT %d   AND CREATED: %d", ppid,pid);
-	}
-    else{
-        PRINT("[!] PROCESS DESTROYED %d", pid);
+    // Ahora toca buscar la DLL correspondiente
+    if (CanBeInjected(info)) {
+
+        SET_UNICODE_STRING(path_dll, NTDLL_NATIVE_PATH);
+
+        /* Aqui debo buscar la DLL entre las DLL que se carguen */
+        if (IsSuffixedUnicodeString(ImageName, &path_dll, TRUE))
+        {
+            // Funcionara?
+            PVOID LdrLoadDllRoutineAddress = RtlxFindExportedRoutineByName(ImageInfo->ImageBase, &LdrLoadDLLRoutineName);
+
+            // Si retorna un valor Nulo estaremos mal
+            if (!LdrLoadDllRoutineAddress) {
+                PRINT("[-] RUTINA NO OBTENIDA");
+                PRINT("[.] Removiendo informacion de este proceso");
+                if (RemoveInfoByProcess(ProcessId)) {
+                    PRINT("[+] Informacion removida");
+                }
+                return;
+            }
+
+            PRINT("[+] Direccion de la funcion LdrDLL obtenida");
+            info->LdrLoadDllRoutineAddress = LdrLoadDllRoutineAddress;
+        }
+
+        return;
     }
 }
+
+
+/**
+ * @brief Rutina para detectar cuando un proceso se crea o se destruye
+ *
+ * @param ParentId Id del proceso padre
+ *
+ * @param ProcessId Id del proceso creado o destruido
+ *
+ * @param create Variable que indica si se crea o termina el proceso
+ */
+void NotifyForCreateAProcess(HANDLE ParentId, HANDLE ProcessId, BOOLEAN create)
+{
+    UNREFERENCED_PARAMETER(ParentId);
+
+    // Se crea un proceso
+    if (create)
+    {
+        // Imprimir si el proceso fue creado
+        PRINT("[+] Se crea un proceso con el pid: %d", ProcessId);
+
+        /* Cuando se crea un proceso se debe crear la informacion que se inserta en la lista */
+        if (NT_SUCCESS(CreateInfo(ProcessId))) {
+            PRINT("[+] Informacion creada");
+        }
+    }
+    else
+    {
+        if (RemoveInfoByProcess(ProcessId)) {
+            PRINT("[+] Info removida correctamente");
+        }
+    }
+}
+
 
 
 // Funcion de finalizado par el driver
 void Unload(PDRIVER_OBJECT DriverObject) {
-	UNREFERENCED_PARAMETER(DriverObject);
+    UNREFERENCED_PARAMETER(DriverObject);
 
-	NTSTATUS status = PsRemoveLoadImageNotifyRoutine(LoadDLLNotify);
+    NTSTATUS status = PsRemoveLoadImageNotifyRoutine(NotifyForAImageLoaded);
 
-	if (!NT_SUCCESS(status)) {
-		PRINT("[-] ERROR REMOVIENDO RUTINA DE CARGA DE DLL: 0x%x",status);
-	}
+    if (!NT_SUCCESS(status)) {
+        PRINT("[-] ERROR REMOVIENDO RUTINA DE CARGA DE DLL: 0x%x", status);
+    }
 
-	status = PsSetCreateProcessNotifyRoutine(CreateNotifyRoutine, TRUE);
+    status = PsSetCreateProcessNotifyRoutine(NotifyForCreateAProcess, TRUE);
 
-	if (!NT_SUCCESS(status))
-	{
-		PRINT("[-] ERROR REMOVIENDO LA RUTINA DE CREACION DE PROCESOS: 0x%x", status);
-	}
+    if (!NT_SUCCESS(status))
+    {
+        PRINT("[-] ERROR REMOVIENDO LA RUTINA DE CREACION DE PROCESOS: 0x%x", status);
+    }
 
-	PRINT("[.] DRIVER UNLOADED");
+    PRINT("[.] DRIVER UNLOADED");
 
 }
 
 extern "C" NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath) {
-	NTSTATUS status = STATUS_SUCCESS;
+    NTSTATUS status = STATUS_SUCCESS;
 
-	// Print logs
-	PRINT("CARGANDO DRIVER");
+    // Print logs
+    PRINT("CARGANDO DRIVER");
 
-	UNREFERENCED_PARAMETER(DriverObject);
-	UNREFERENCED_PARAMETER(RegistryPath);
+    UNREFERENCED_PARAMETER(DriverObject);
+    UNREFERENCED_PARAMETER(RegistryPath);
 
-	status = PsSetLoadImageNotifyRoutine(LoadDLLNotify);
+    status = PsSetLoadImageNotifyRoutine(NotifyForAImageLoaded);
 
-	if (!NT_SUCCESS(status)) {
-		PRINT("[-] ERROR 0x%x", status);
-		return status;
-	}
+    if (!NT_SUCCESS(status)) {
+        PRINT("[-] ERROR 0x%x", status);
+        return status;
+    }
 
-	status = PsSetCreateProcessNotifyRoutine(CreateNotifyRoutine, FALSE);
+    status = PsSetCreateProcessNotifyRoutine(NotifyForCreateAProcess, FALSE);
 
-	if (!NT_SUCCESS(status)) {
-		PsRemoveLoadImageNotifyRoutine(LoadDLLNotify);
-		PRINT("[-] ERROR 0x%x", status);
+    if (!NT_SUCCESS(status)) {
+        PsRemoveLoadImageNotifyRoutine(NotifyForAImageLoaded);
+        PRINT("[-] ERROR 0x%x", status);
 
-		return status;
-	}
+        return status;
+    }
 
-	// Asignamos la funcion de Descarga del Driver
-	DriverObject->DriverUnload = Unload;
+    // Asignamos la funcion de Descarga del Driver
+    DriverObject->DriverUnload = Unload;
 
-	PRINT("[+] DRIVER CARGADO");
+    PRINT("[+] DRIVER CARGADO");
 
-	return status;
+    return status;
 }
